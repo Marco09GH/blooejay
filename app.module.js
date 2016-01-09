e0574c4fb1e985cc0fb6256b82a5ca79bae532bf
Marek Pietrzak <marek.pietrzak@actech.pl>
2016-01-09 12:13:01 +0100

    mongo added

--- app.module.js
+++ app.module.js
@@ -1,1 +1,6 @@
 var restify = require('restify');
+var mongo = require('mongodb').MongoClient;
+
+mongo.connect('mongodb://localhost:27017/video', function (err, db) {
+    var server = restify.createServer();
+});
