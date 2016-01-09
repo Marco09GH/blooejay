edfed0bb176f66fec1a596445349088de8c206ec
Marek Pietrzak <marek.pietrzak@actech.pl>
2016-01-09 13:19:07 +0100

    added tsd

--- app.module.js
+++ app.module.js
@@ -3,5 +3,14 @@ var mongo = require('mongodb').MongoClient;
 
 mongo.connect('mongodb://localhost:27017/video', function (err, db) {
     var server = restify.createServer();
+    server.get('/api/get_movies', function(req, res, next) {
+        db.collection('movies').find({}).toArray(function(err, docs) {
+            res.send(docs);
+            next();
+        });
+    });
     
+    server.listen(3000, function() {
+       console.log('%s listening at %s', server.name, server.url); 
+    });
 });
---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="bunyan/bunyan.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="restify/restify.d.ts" />
+/// <reference path="mongoose/mongoose.d.ts" />
+/// <reference path="mongodb/mongodb.d.ts" />
